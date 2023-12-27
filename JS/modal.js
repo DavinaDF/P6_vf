6af2be3a6d215a8f7a3226c99cc1bbd9f6ad7a9a
Davina Crahet <davina.crahet@gmail.com>
2023-12-28 00:27:37 +0100

    Test commit

--- JS/modal.js
+++ JS/modal.js
@@ -3,10 +3,12 @@ const editButton = document.querySelector(".open_modal");
 const buttonAddWork = document.querySelector(".button_add");
 const modalDeleteWork = document.getElementById("delete_work");
 const modalAddWork = document.getElementById("add_work");
+const arrowBack = document.querySelector(".fa-arrow-left");
+const buttonUploadPhoto = document.querySelector(".upload_photo");
 
 // Apparition de la modal
 const modal = document.querySelector("aside");
-const faClose = document.querySelector(".close");
+const faClose = document.querySelectorAll(".close");
 console.log(faClose);
 
 function openModal(e) {
@@ -19,12 +21,21 @@ function openModal(e) {
 function closeModal(e) {
   e.preventDefault();
   modal.style.display = "none";
+  modalDeleteWork.style.display = null;
+  modalAddWork.style.display = "none";
   modal.setAttribute("aria-hidden", "true");
   modal.removeAttribute("aria-modal");
 }
 
 editButton.addEventListener("click", openModal);
-faClose.addEventListener("click", closeModal);
+faClose.forEach(function (xmarkClose) {
+  xmarkClose.addEventListener("click", closeModal);
+});
+// faClose.addEventListener("click", closeModal);
+arrowBack.addEventListener("click", function () {
+  modalDeleteWork.style.display = null;
+  modalAddWork.style.display = "none";
+});
 
 getWorks().then((works) => {
   displayWorksModal(works);
@@ -38,12 +49,15 @@ function displayWorksModal(works) {
 
     const workElement = document.createElement("figure");
     const workImage = document.createElement("img");
+    const workIconDelete = document.createElement("i");
+    workIconDelete.classList.add("fa-solid", "fa-trash-can");
     workImage.src = work.imageUrl;
 
     // On rattache la balise figure à la section Gallery
     galleryModal.appendChild(workElement);
     // On rattache l’image et la légende à projectElement (la balise figure)
     workElement.appendChild(workImage);
+    workElement.appendChild(workIconDelete);
   }
 }
 
